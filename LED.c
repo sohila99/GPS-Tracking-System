eb7f7a29f916556ab96e73689c77c13fc78968d4
Ahmed Yasser Kamal Kaead <85291999+AhmedYasserKaead@users.noreply.github.com>
2021-06-05 17:03:51 +0200

    Update LED.c

--- LED.c
+++ LED.c
@@ -1,18 +1,6 @@
 
 #include "tm4c123gh6pm_registers.h"
 
-/* Enable PF0 (SW2) */
-void SW2_Init(void)
-{
-    GPIO_PORTF_LOCK_REG   = 0x4C4F434B;   /* Unlock the GPIO_PORTF_CR_REG */
-    GPIO_PORTF_CR_REG    |= (1<<0);       /* Enable changes on PF0 */
-    GPIO_PORTF_AMSEL_REG &= ~(1<<0);      /* Disable Analog on PF0 */
-    GPIO_PORTF_PCTL_REG  &= 0xFFFFFFF0;   /* Clear PMCx bits for PF0 to use it as GPIO pin */
-    GPIO_PORTF_DIR_REG   &= ~(1<<0);      /* Configure PF0 as input pin */
-    GPIO_PORTF_AFSEL_REG &= ~(1<<0);      /* Disable alternative function on PF0 */
-    GPIO_PORTF_PUR_REG   |= (1<<0);       /* Enable pull-up on PF0 */
-    GPIO_PORTF_DEN_REG   |= (1<<0);       /* Enable Digital I/O on PF0 */
-}
 
 /* Enable PF1 (RED LED) */
 void Led_Red_Init(void)
@@ -27,23 +15,22 @@ void Led_Red_Init(void)
 
 int main(void)
 {
+
     /* Enable clock for PORTF and allow time for clock to start*/
     volatile unsigned long delay = 0;
     SYSCTL_REGCGC2_REG |= 0x00000020;
     delay = SYSCTL_REGCGC2_REG;
 
+
     /* Initailize the SW2 and Red LED as GPIO Pins */
-    SW2_Init();
-    Led_Red_Init();
+    Led_Red_Init(); 
 
-    double X = get_distance;
+    double X ;
+	
 
-    while(1)
-    {
-        if(X==100)
+        if(X >= 100)
         {
-
-            GPIO_PORTF_DATA_REG |= (1<<1);  /* LED ON */
+            GPIO_PORTF_DATA_REG |= 0x00000020;  /* LED RED ON */
         }
-    }
+    
 }
