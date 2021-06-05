aa306ba40b247b8670f4e74ad6e4a45ac64e8e4a
NourAtefMohamedMohamed <85291183+NourAtefMohamedMohamed@users.noreply.github.com>
2021-06-05 23:51:08 +0200

    Update Distance.c

--- Distance.c
+++ Distance.c
@@ -36,6 +36,13 @@ long double dist(double lat1, double lon1, double lat2, double lon2);
     }
 }
 
+/* function to wait for the clock */
+
+void wait() {
+	int clockCounter = 0;
+	while (clockCounter++ < 1000000) {}
+}
+
 int main(void)
 {
     double get_distance;
@@ -43,28 +50,37 @@ int main(void)
     double lon_new; /* the longitude of the current point the GPS locating */
     double lat_old; /* the latitude of the previous point that the GPS located */
     double lon_old; /* the longitude of the previous point that the GPS located*/
+    double GPS_lat;
+    double GPS_lon;
     double distance; /* distance between the current point and the point before it */
     double get_distance; /* total distance */
-   
+    wait();
     while (1)
     {
         if (GPS_lat > 30.063856 && GPS_lat < 30.063940 && GPS_lon > 31.277541 && GPS_lon < 31.277653) /* margin for starting point */
         {
+            scanf_s("%lf", &GPS_lat);
+            scanf_s("%lf", &GPS_lon);
             lat_old = GPS_lat;
             lon_old = GPS_lon; /* initialize latOld and lonOld with GPS first coordinates */
             get_distance = 0; /* reset total distance to 0 */
             while (get_distance <= 100)
             {
+                scanf_s("%lf", &GPS_lat);
+                scanf_s("%lf", &GPS_lon);
                 lat_new = GPS_lat; /* reading latitude from GPS */
                 lon_new = GPS_lon; /* reading longitude from GPS */
                 distance = dist(lat_old, lon_old, lat_new, lon_new); /* calculate distance between current point and previous one */
                 get_distance = get_distance + distance; /* add the distance to the total distance */
                 lon_old = lon_new;
                 lat_old = lat_new; /* make the current point the previous one for the next iteration of the loop */
+                printf("%lf", get_distance);
+                wait();
             }
             double destination_lat = lat_new;
             double destination_lon = lon_new;         
         }
     }
+    return 0;
 }
 
