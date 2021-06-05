498c705a7e614b88951865c5e146435f87b92258
Ahmed Yasser Kamal Kaead <85291999+AhmedYasserKaead@users.noreply.github.com>
2021-06-05 19:42:14 +0200

    Update LED.c

--- LED.c
+++ LED.c
@@ -5,32 +5,31 @@
 /* Enable PF1 (RED LED) */
 void Led_Red_Init(void)
 {
-    GPIO_PORTF_AMSEL_REG &= ~(1<<1);      /* Disable Analog on PF1 */
-    GPIO_PORTF_PCTL_REG  &= 0xFFFFFF0F;   /* Clear PMCx bits for PF1 to use it as GPIO pin */
-    GPIO_PORTF_DIR_REG   |= (1<<1);       /* Configure PF1 as output pin */
-    GPIO_PORTF_AFSEL_REG &= ~(1<<1);      /* Disable alternative function on PF1 */
-    GPIO_PORTF_DEN_REG   |= (1<<1);       /* Enable Digital I/O on PF1 */
-    GPIO_PORTF_DATA_REG  &= ~(1<<1);      /* Clear bit 1 in Data regsiter to turn off the led */
-}
+	SYSCTL_REGCGC2_REG |= 0x00000020;
+        GPIO_PORTF_LOCK_REG = 0x4C4F434B;
+	GPIO_PORTF_CR_REG =0x1F;
+	GPIO_PORTF_AFSEL_REG =0;
+	GPIO_PORTF_PCTL_REG =0;
+	GPIO_PORTF_AMSEL_REG=0;
+	GPIO_PORTF_DIR_REG=0X0E;
+	GPIO_PORTF_PUR_REG=0X11;
+	GPIO_PORTF_DEN_REG =0X1F;
 
+}
+double X;
+
+void Turn_On(X)
+{
+	if (X>=100)
+	{
+		GPIO_PORTF_DATA_REG |= (1<<1);
+	}
+}
 int main(void)
 {
-
-    /* Enable clock for PORTF and allow time for clock to start*/
-    volatile unsigned long delay = 0;
-    SYSCTL_REGCGC2_REG |= 0x00000020;
-    delay = SYSCTL_REGCGC2_REG;
-
-
-    /* Initailize the SW2 and Red LED as GPIO Pins */
-    Led_Red_Init(); 
-
-    double X ;
-	
-
-        if(X >= 100)
-        {
-            GPIO_PORTF_DATA_REG |= 0x00000020;  /* LED RED ON */
-        }
-    
+	while (1)
+		{
+			Led_Red_Init();  
+			Turn_On(200);			
+		}
 }
