c1c1f9dfcbcc4ecf1784f4e5dcf5b82adca98f7f
NourAtefMohamedMohamed <85291183+NourAtefMohamedMohamed@users.noreply.github.com>
2021-06-05 22:58:31 +0200

    Update Distance.c

--- Distance.c
+++ Distance.c
@@ -1,24 +1,10 @@
 
 #include <stdio.h>
 #include <math.h>
-#include "tm4c123gh6pm_registers.h"
 
 #define PI = 3.141592653589;
 
 using namespace std;
-
-/* Enable PF0 (SW) */
-void SW_Init(void)
-{
-    GPIO_PORTF_LOCK_REG = 0x4C4F434B;   /* Unlock the GPIO_PORTF_CR_REG */
-    GPIO_PORTF_CR_REG |= (1 << 0);       /* Enable changes on PF0 */
-    GPIO_PORTF_AMSEL_REG &= ~(1 << 0);      /* Disable Analog on PF0 */
-    GPIO_PORTF_PCTL_REG &= 0xFFFFFFF0;   /* Clear PMCx bits for PF0 to use it as GPIO pin */
-    GPIO_PORTF_DIR_REG &= ~(1 << 0);      /* Configure PF0 as input pin */
-    GPIO_PORTF_AFSEL_REG &= ~(1 << 0);      /* Disable alternative function on PF0 */
-    GPIO_PORTF_PUR_REG |= (1 << 0);       /* Enable pull-up on PF0 */
-    GPIO_PORTF_DEN_REG |= (1 << 0);       /* Enable Digital I/O on PF0 */
-}
 
 /* function to convert from degree to radian */
 
@@ -53,33 +39,31 @@ long double dist(double lat1, double lon1, double lat2, double lon2);
 int main(void)
 {
     double get_distance;
-    double latNew; /* the latitude of the current point the GPS is locating*/
-    double lonNew; /* the longitude of the current point the GPS locating */
-    double latOld; /* the latitude of the previous point that the GPS located */
-    double lonOld; /* the longitude of the previous point that the GPS located*/
+    double lat_new; /* the latitude of the current point the GPS is locating*/
+    double lon_new; /* the longitude of the current point the GPS locating */
+    double lat_old; /* the latitude of the previous point that the GPS located */
+    double lon_old; /* the longitude of the previous point that the GPS located*/
     double distance; /* distance between the current point and the point before it */
     double get_distance; /* total distance */
-    SW_Init(); /* Initialize the SW switch as GPIO Pin */
-
-   /* double slope[]; */
-   /* slope[i] = (latitude[i + 1] - latitude[i]) / (longitude[i + 1] - longitude[i]); */
-    
+   
     while (1)
     {
-        if (!(GPIO_PORTF_DATA_REG & (1 << 0))) /* if switch is on,  start calculating distance */
+        if (GPS_lat > 30.063856 && GPS_lat < 30.063940 && GPS_lon > 31.277541 && GPS_lon < 31.277653) /* margin for starting point */
         {
-            latOld = GPS_lat;
-            lonOld = GPS_lon; /* initialize latOld and lonOld with GPS first coordinates */
+            lat_old = GPS_lat;
+            lon_old = GPS_lon; /* initialize latOld and lonOld with GPS first coordinates */
             get_distance = 0; /* reset total distance to 0 */
             while (get_distance <= 100)
             {
-                latNew = GPS_lat; /* reading latitude from GPS */
-                lonNew = GPS_lon; /* reading longitude from GPS */
-                distance = dist(latOld, lonOld, latNew, lonNew); /* calculate distance between current point and previous one */
+                lat_new = GPS_lat; /* reading latitude from GPS */
+                lon_new = GPS_lon; /* reading longitude from GPS */
+                distance = dist(lat_old, lon_old, lat_new, lon_new); /* calculate distance between current point and previous one */
                 get_distance = get_distance + distance; /* add the distance to the total distance */
-                lonOld = lonNew;
-                latOld = latNew; /* make the current point the previous one for the next iteration of the loop */
+                lon_old = lon_new;
+                lat_old = lat_new; /* make the current point the previous one for the next iteration of the loop */
             }
+            double destination_lat = lat_new;
+            double destination_lon = lon_new;         
         }
     }
 }
