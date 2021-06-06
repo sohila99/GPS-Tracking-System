613b52002162762eb9d4ebb747a11ac7a8241d99
Sohila Noureldin <53787050+sohila99@users.noreply.github.com>
2021-06-06 20:36:13 +0200

    Update LED.c

--- LED.c
+++ LED.c
@@ -5,15 +5,15 @@
 /* Enable PF1 (RED LED) */
 void Led_Red_Init(void)
 {
-	SYSCTL_REGCGC2_REG |= 0x00000020;
-  	GPIO_PORTF_LOCK_REG = 0x4C4F434B;
-	GPIO_PORTF_CR_REG =0x1F;
-	GPIO_PORTF_AFSEL_REG =0;
-	GPIO_PORTF_PCTL_REG =0;
-	GPIO_PORTF_AMSEL_REG=0;
-	GPIO_PORTF_DIR_REG=0X0E;
-	GPIO_PORTF_PUR_REG=0X11;
-	GPIO_PORTF_DEN_REG =0X1F;
+	SYSCTL_RCGC2_R |= 0x00000020;
+  	GPIO_PORTF_LOCK_R = 0x4C4F434B;
+	GPIO_PORTF_CR_R =0x1F;
+	GPIO_PORTF_AFSEL_R =0;
+	GPIO_PORTF_PCTL_R =0;
+	GPIO_PORTF_AMSEL_R=0;
+	GPIO_PORTF_DIR_R=0X0E;
+	GPIO_PORTF_PUR_R=0X11;
+	GPIO_PORTF_DEN_R =0X1F;
 
 }
 double distance;
@@ -22,7 +22,7 @@ void Turn_On(double distance)
 {
 	if (distance>=100)
 	{
-		GPIO_PORTF_DATA_REG |= (1<<1);
+		GPIO_PORTF_DATA_R |= (1<<1);
 	}
 }
 int main(void)
@@ -30,6 +30,6 @@ int main(void)
 	while (1)
 		{
 			Led_Red_Init();  
-			Turn_On(90);			
+			Turn_On(150);			
 		}
 }
