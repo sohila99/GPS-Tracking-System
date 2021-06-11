ed4805b10ced07c018ce5f37239e44b0a1e44231
NourAtefMohamedMohamed <85291183+NourAtefMohamedMohamed@users.noreply.github.com>
2021-06-11 14:07:05 +0200

    Update ASCII.c

--- ASCII.c
+++ ASCII.c
@@ -1,5 +1,23 @@
-
-ASCII_dist(double distance)
+ASCII_dist_0(double dist)
 {
-  
+ if ( dist < 0 )
+   
 }
+
+ASCII_dist_1(double dist)
+{
+ if ( dist < 0 )
+   
+}
+
+ASCII_dist_2(double dist)
+{
+ if ( dist < 0 )
+   
+}
+
+ASCII_dist_3(double dist)
+{
+ if ( dist < 0 )
+}
+
