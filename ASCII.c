616cc9d3c43603977e8f0fa0d0533e34d447d247
NourAtefMohamedMohamed <85291183+NourAtefMohamedMohamed@users.noreply.github.com>
2021-06-11 17:54:17 +0200

    Update ASCII.c

--- ASCII.c
+++ ASCII.c
@@ -15,19 +15,14 @@ dist_ASCII_b0(double d)
 dist_ASCII_b1(double d)
 {
  int a;
- int b;
  if ( d < 0 )
  {
    return 0x49; // if below 0, print "I" of the word "MIN"
  }
- elseif (d < 10 && d >= 0)
+ elseif (d >= 0 && d < 1000)
  {
-  a = d + 0x30; // if the number is already 1-digit, print it
-  return a;
- }
- eiseif (d < 1000 && d >= 10)
- {
-  a = d % 10; // if the number is 2-digits or 3-digits, a = units digit
+  a = d % 10; // a = units digit
+  a = a + 0x30;
   return a;
  }
  elseif (d >= 1000)
@@ -35,30 +30,37 @@ dist_ASCII_b1(double d)
   return 0x41; // if above 3-digits, print "A" of the word "MAX"
  }
 }
-//stop
+
 dist_ASCII_b2(double d)
 {
  int a;
  int b;
- if ( d < 0 || d >= 10000)
+ if (d >= 1000 || d < 0)
  {
   return 0x4D; // if below 0 or above 3-digits, print "M" of the word "MAX" or "MIN"
  }
- elseif ( d >= 0 && d < 10)
+ elseif (d >= 0 && d < 1000)
  {
-  return 0x30; // if the numbeer is 1-digit, then tens digit = 0
+  a = d % 100; // a = rightmost 2 digits
+  b = d % 10; // b = units digit
+  a = d - b; // tenth digit
+  a = a + 0x30;
+  return a;
  }
- elseif ( d >= 10 && d < 100)
- {
-  
- 
-   
-}
 
 dist_ASCII_b3(double d)
 {
  int a;
  int b;
- if ( d < 0 )
+ if (d >= 1000 || d < 0)
+ {
+  return 0x00; // print empty space since "MAX" and "MIN" need only 3 spaces
+ }
+ if (d >= 0 && d < 1000)
+ {
+  b = d % 100; // b = rightmost 2 digits
+  a = d - b; // a = 3rd digit
+  a = a + 0x30;
+  return a;
 }
 
