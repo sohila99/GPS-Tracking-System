b1e1cfcec724b03248091927be64b79c8f7c3afe
NourAtefMohamedMohamed <85291183+NourAtefMohamedMohamed@users.noreply.github.com>
2021-06-11 17:11:57 +0200

    Update ASCII.c

--- ASCII.c
+++ ASCII.c
@@ -2,38 +2,46 @@ dist_ASCII_b0(double d)
 {
  if ( d < 0 )
  {
-  return 0;
+  return 0x4E; // if below 0, print "N" of the word "MIN"
  }
- else
+ elseif ( d >= 0 && d < 1000)
  {
-  return 0x6D;
+  return 0x6D; // if in the correct range, print "m" for meters
  }
+ elseif (d >= 1000)
+  return 0x58; // if above 3-digits, print "X" of the word "MAX"
 }
 
 dist_ASCII_b1(double d)
 {
  int a;
+ int b;
  if ( d < 0 )
  {
-   return 0;
+   return 0x49; // if below 0, print "I" of the word "MIN"
  }
  elseif (d < 10 && d >= 0)
  {
-  a = d + 0x30;
+  a = d + 0x30; // if the number is already 1-digit, print it
   return a;
  }
- eiseif (d >= 10)
+ eiseif (d < 1000 && d >= 10)
  {
-  while (d > 0)
-  {
-   
-  }
+  b = d / 10; // if the number is 2-digits or 3-digits, b = tens (and hundreds) digit
+  a = d - 10*b; // a = units digit
+  return a;
+ }
+ elseif (d >= 1000)
+ {
+  return 0x41; // if above 3-digits, print "A" of the word "MAX"
  }
 }
 
 dist_ASCII_b2(double d)
 {
- if ( d < 0 )
+ if ( d < 0 || d >= 10000)
+ {
+  return 0x4D; // if below 0 or above 3-digits, print "M" of the word "MAX" or "MIN"
    
 }
 
