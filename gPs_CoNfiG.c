9cd251b43cb6ef06e28a0fe596645c5b44cf0d94
Ahmed Yasser Kamal Kaead <85291999+AhmedYasserKaead@users.noreply.github.com>
2021-06-11 15:08:17 +0200

    Update gPs_CoNfiG.c

--- gPs_CoNfiG.c
+++ gPs_CoNfiG.c
@@ -1,9 +1,24 @@
+#include "tm4c123gh6pm_registers.h"
+#include "stdint.h"
 
-void GPS_INIT(void)
+
+void GPS_Init(void)
 {
-SYSCTL_RCGCUART_R |= 0x04;
-SYSCTL_RCGCGPIO_R |= 0x04;
-GPIO_PORTC_AFSEL_R |= 0x30;
-GPIO_PORTC_PCTL_R &= 0xFF00FFFF;
-GPIO_PORTC_PCTL_R |= 0x00220000;
+	SYSCTL_RCGCUART_R |= 0x04;
+	SYSCTL_RCGCGPIO_R |= 0x04;
+	
+	UART0_CTL_R &= ~
+	
+	
+	
+	
+
+
+
+	GPIO_PORTC_AFSEL_R |= 0x30;
+	GPIO_PORTC_PCTL_R &= 0xFF00FFFF;
+	GPIO_PORTC_PCTL_R |= 0x00220000;
+	GPIO_PORTC_DEN_R |= 0x30;
+
+
 }
