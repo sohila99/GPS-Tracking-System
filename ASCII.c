4eccb681490b277fd2476df4614b5997185b4de5
NourAtefMohamedMohamed <85291183+NourAtefMohamedMohamed@users.noreply.github.com>
2021-06-11 14:20:33 +0200

    Update ASCII.c

--- ASCII.c
+++ ASCII.c
@@ -1,23 +1,44 @@
-ASCII_dist_0(double dist)
+dist_ASCII_b0(double d)
 {
- if ( dist < 0 )
+ if ( d < 0 )
+ {
+  return 0;
+ }
+ else
+ {
+  return 0x6D;
+ }
+}
+
+dist_ASCII_b1(double d)
+{
+ int a;
+ if ( d < 0 )
+ {
+   return 0;
+ }
+ elseif (d < 10 && d >= 0)
+ {
+  a = d + 0x30;
+  return a;
+ }
+ eiseif (d >= 10)
+ {
+  while (d > 0)
+  {
+   
+  }
+ }
+}
+
+dist_ASCII_b2(double d)
+{
+ if ( d < 0 )
    
 }
 
-ASCII_dist_1(double dist)
+dist_ASCII_b3(double d)
 {
- if ( dist < 0 )
-   
-}
-
-ASCII_dist_2(double dist)
-{
- if ( dist < 0 )
-   
-}
-
-ASCII_dist_3(double dist)
-{
- if ( dist < 0 )
+ if ( d < 0 )
 }
 
