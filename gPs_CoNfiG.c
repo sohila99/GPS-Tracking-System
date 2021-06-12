562e322b8029aaf9bfa54ebfd931042ad8ad9bc0
Ahmed Yasser Kamal Kaead <85291999+AhmedYasserKaead@users.noreply.github.com>
2021-06-12 16:55:56 +0200

    Update gPs_CoNfiG.c

--- gPs_CoNfiG.c
+++ gPs_CoNfiG.c
@@ -18,3 +18,14 @@ void GPS_Init(void)
 	GPIO_PORTC_PCTL_R |= 0x00220000;
 	GPIO_PORTC_DEN_R |= 0x30;
 }
+
+uint8_t GPS_Data_Available(void)
+{
+	return ((UART1_FR_R & 0x00000010) == 0x00000010) ? 0 : 1;
+}
+
+uint8_t GPS_Receive(void)
+{
+	while (UART1_Available() != 1);
+	return (uint8_t)(UART1_DR_R & 0xFF);
+}
