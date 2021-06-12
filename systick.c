65f43cfaa574c9984fef67adc65eb558f2eb1e68
Sohila Noureldin <53787050+sohila99@users.noreply.github.com>
2021-06-12 21:10:56 +0200

    Update systick.c

--- systick.c
+++ systick.c
@@ -1,1 +1,21 @@
+void systick_init()
+{
+  NVIC_ST_CTRL_R=0; // disable timer
+  NVIC_ST_RELOAD_R = 16000-1; // delay time is 1 ms or 16000-1 counts
+  NVIC_ST_CURRENT_R = 0; // clear current value & flag
+  NVIC_ST_CTRL_R=5; // enable timer
+}
 
+int main ()
+{
+  systick_init();
+  while (1)
+  {
+   if (NVIC_ST_CTRL_R&0x10000)
+   {
+     
+   }
+  }
+
+
+}
